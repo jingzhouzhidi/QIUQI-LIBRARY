750abe0e630d3d3278846727c9554dd8603c8d64
textGamex <textGamex@outlook.com>
2025-05-05 21:50:48 +0800

    25.5.5更新

---
+++ 代码工具/MOD文件夹生成.bat
@@ -0,0 +1,116 @@
+::墨合-文件夹生成工具
+::垃圾批发喵
+::捅死你捅死你捅死你捅死你捅死你捅死你捅死你捅死你捅死你
+::捅死你捅死你捅死你捅死你捅死你捅死你捅死你捅死你捅死你
+::捅死你捅死你捅死你捅死你捅死你捅死你捅死你捅死你捅死你
+::捅死你捅死你捅死你捅死你捅死你捅死你捅死你捅死你捅死你
+::捅死你捅死你捅死你捅死你捅死你捅死你捅死你捅死你捅死你
+::捅死你捅死你捅死你捅死你捅死你捅死你捅死你捅死你捅死你
+::捅死你捅死你捅死你捅死你捅死你捅死你捅死你捅死你捅死你
+::捅死你捅死你捅死你捅死你捅死你捅死你捅死你捅死你捅死你
+::捅死你捅死你捅死你捅死你捅死你捅死你捅死你捅死你捅死你
+MD	common
+MD	events
+MD	gfx
+MD	history
+MD	interface
+MD	localisation
+MD	common\abilities
+MD	common\aces
+MD	common\ai_areas
+MD	common\ai_equipment
+MD	common\ai_focuses
+MD	common\ai_strategy
+MD	common\ai_strategy_plans
+MD	common\ai_templates
+MD	common\autonomous_states
+MD	common\bookmarks
+MD	common\bop
+MD	common\buildings
+MD	common\characters
+MD	common\continuous_focus
+MD	common\countries
+MD	common\country_leader
+MD	common\country_tag_aliases
+MD	common\country_tags
+MD	common\decisions
+MD	common\defines
+MD	common\difficulty_settings
+MD	common\dynamic_modifiers
+MD	common\equipment_groups
+MD	common\focus_inlay_windows
+MD	common\game_rules
+MD	common\generation
+MD	common\idea_tags
+MD	common\ideas
+MD	common\ideologies
+MD	common\intelligence_agencies
+MD	common\intelligence_agency_upgrades
+MD	common\map_modes
+MD	common\medals
+MD	common\military_industrial_organization
+MD	common\modifier_definitions
+MD	common\modifiers
+MD	common\mtth
+MD	common\names
+MD	common\national_focus
+MD	common\occupation_laws
+MD	common\on_actions
+MD	common\operation_phases
+MD	common\operation_tokens
+MD	common\operations
+MD	common\opinion_modifiers
+MD	common\peace_conference
+MD	common\profile_backgrounds
+MD	common\profile_pictures
+MD	common\raids
+MD	common\resistance_activity
+MD	common\resistance_compliance_modifiers
+MD	common\resources
+MD	common\ribbons
+MD	common\scientist_traits
+MD	common\scorers
+MD	common\script_constants
+MD	common\scripted_diplomatic_actions
+MD	common\scripted_effects
+MD	common\scripted_guis
+MD	common\scripted_localisation
+MD	common\scripted_triggers
+MD	common\special_projects
+MD	common\state_category
+MD	common\technologies
+MD	common\technology_sharing
+MD	common\technology_tags
+MD	common\terrain
+MD	common\timed_activities
+MD	common\unit_leader
+MD	common\unit_medals
+MD	common\unit_tags
+MD	common\units
+MD	common\wargoals
+MD	common\decisions\categories
+MD	common\military_industrial_organization\ai_bonus_weights
+MD	common\military_industrial_organization\organizations
+MD	common\military_industrial_organization\policies
+MD	common\peace_conference\ai_peace
+MD	common\peace_conference\categories
+MD	common\peace_conference\cost_modifiers
+MD	common\raids\categories
+MD	common\scorers\country
+MD	common\special_projects\project_tags
+MD	common\special_projects\projects
+MD	common\special_projects\prototype_rewards
+MD	common\special_projects\specialization
+MD	common\units\codenames_operatives
+MD	common\units\critical_parts
+MD	common\units\equipment
+MD	common\units\names
+MD	common\units\names_divisions
+MD	common\units\names_railway_guns
+MD	common\units\names_ships
+MD	common\units\unit_modifiers
+MD	history\countries
+MD	history\general
+MD	history\states
+MD	history\units
+MD	localisation\simp_chinese
